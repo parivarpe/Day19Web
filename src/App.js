39037b45eb48be4290cb14dc2aa8f793a4a3616f
parivarpe <parivarpe01@gmail.com>
2022-01-04 15:55:53 +0530

    small change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ *
+ * @returns
+ */
+let list1 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header1></Header1>
+      <div className="container-fluid">
+        <div className="row">
+          {list1.map(() => (
+            <div className="col-sm-12 col-md-4 mt-2">
+              <CardDesign></CardDesign>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export function Header1() {
+  return (
+    <div className=" container-fluid sticky-top">
+      <div className="row bg-secondary  text-light p-3">
+        <div className="col-12 ">
+          <h3>My Gallary</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function CardDesign() {
+  let cardtitle = "MyPhoto";
+  return (
+    <div>
+      <div className="card">
+        <div className="card-header"> {cardtitle}</div>
+        <div className="card-body">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+          expedita nostrum distinctio necessitatibus eaque ad perspiciatis in
+          repellat fuga impedit.
+        </div>
+        <div className="card-footer">
+          <input
+            type="button"
+            value="Add To Cart"
+            className="btn btn-success"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
